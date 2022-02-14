5a15eb470d81e27429b7bc99afd8f88093559fac
Généreux akotenou <genereux.akotenou@gmail.com>
2022-02-14 03:08:30 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,379 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <!-- basic config  -->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- webpage icon  -->
+    <link rel="icon" type="image/png" sizes="48x48" href="/favicon.png">
+    <!-- Style sheet   -->
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/gQuick.css">
+    <link rel="stylesheet" href="css/responsive.css">
+    <!-- Icon package  -->
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <!-- Scripts link  -->
+    <script src="script/my_script.js"></script>
+    <!-- window title  -->
+    <title>webMeteo - AI&IoT Club</title>
+    <!-- 20210914_0921 -->
+</head>
+<body>
+    <header>
+        <a href="">GET DATASET</a>
+    </header>
+    <main>
+        <!-- main block -->
+        <div id="mainBloc1">
+            <div class="gFlex gFlex-column blocTemp1">
+                <p><b><i class="fa fa-map-marker"></i> Calavi</b></p>
+                <p>
+                    dim. 13 févier 00:00
+                </p>
+            </div>
+            <div class="gFlex gFlex-row blocTemp">
+                <div>
+                    <img width="100px" src="./img/wt-2.svg" alt="">
+                </div>
+                <div>
+                    <p>28°c</p>
+                </div>
+            </div>
+            <div class="gFlex gFlex-row blocTemp2">
+                <span>Clair</span>
+            </div>
+        </div>
+
+        <!-- data per hours -->
+        <div id="mainBloc2" class="lotOfData">
+            <span class="subBlocTitle">Température</span>
+            <div class="TimeCart">
+                <div class="detailBox">
+                    <div class="data">
+                        <b class="m1"></b>
+                        <b class="m2"><span>28°</span><span></span></b>
+                        <b class="m3"></b>
+                    </div>
+                    <div class="time">
+                        <img  width="45px" src="./img/wt-13.svg" alt="">
+                        <p>00:00</p>
+                        <span><i class="fa fa-umbrella"></i> 10%</span>
+                    </div>
+                </div>
+                <div class="detailBox">
+                    <div class="data">
+                        <b class="m1"><span>29°</span><span></span></b>
+                        <b class="m2"></b>
+                        <b class="m3"></b>
+                    </div>
+                    <div class="time">
+                        <img  width="45px" src="./img/wt-13.svg" alt="">
+                        <p>03:00</p>
+                        <span><i class="fa fa-umbrella"></i> 10%</span>
+                    </div>
+                </div>
+                <div class="detailBox">
+                    <div class="data">
+                        <b class="m1"></b>
+                        <b class="m2"></b>
+                        <b class="m3"><span>27°</span><span></span></b>
+                    </div>
+                    <div class="time">
+                        <img  width="45px" src="./img/wt-2.svg" alt="">
+                        <p>06:00</p>
+                        <span><i class="fa fa-umbrella"></i> 10%</span>
+                    </div>
+                </div>
+                <div class="detailBox">
+                    <div class="data">
+                        <b class="m1"></b>
+                        <b class="m2"></b>
+                        <b class="m3"><span>27°</span><span></span></b>
+                    </div>
+                    <div class="time">
+                        <img  width="45px" src="./img/wt-2.svg" alt="">
+                        <p>09:00</p>
+                        <span><i class="fa fa-umbrella"></i> 10%</span>
+                    </div>
+                </div>
+                <div class="detailBox">
+                    <div class="data">
+                        <b class="m1"></b>
+                        <b class="m2"></b>
+                        <b class="m3"><span>27°</span><span></span></b>
+                    </div>
+                    <div class="time">
+                        <img  width="45px" src="./img/wt-1.svg" alt="">
+                        <p>12:00</p>
+                        <span><i class="fa fa-umbrella"></i> 10%</span>
+                    </div>
+                </div>
+                <div class="detailBox">
+                    <div class="data">
+                        <b class="m1"></b>
+                        <b class="m2"></b>
+                        <b class="m3"><span>27°</span><span></span></b>
+                    </div>
+                    <div class="time">
+                        <img  width="45px" src="./img/wt-2.svg" alt="">
+                        <p>15:00</p>
+                        <span><i class="fa fa-umbrella"></i> 10%</span>
+                    </div>
+                </div>
+                <div class="detailBox">
+                    <div class="data">
+                        <b class="m1"></b>
+                        <b class="m2"></b>
+                        <b class="m3"><span>27°</span><span></span></b>
+                    </div>
+                    <div class="time">
+                        <img  width="45px" src="./img/wt-2.svg" alt="">
+                        <p>18:00</p>
+                        <span><i class="fa fa-umbrella"></i> 10%</span>
+                    </div>
+                </div>
+                <div class="detailBox">
+                    <div class="data">
+                        <b class="m1"></b>
+                        <b class="m2"></b>
+                        <b class="m3"><span>27°</span><span></span></b>
+                    </div>
+                    <div class="time">
+                        <img  width="45px" src="./img/wt-6.svg" alt="">
+                        <p>00:00</p>
+                        <span><i class="fa fa-umbrella"></i> 10%</span>
+                    </div>
+                </div>
+                <div class="detailBox">
+                    <div class="data">
+                        <b class="m1"></b>
+                        <b class="m2"></b>
+                        <b class="m3"><span>27°</span><span></span></b>
+                    </div>
+                    <div class="time">
+                        <img  width="45px" src="./img/wt-6.svg" alt="">
+                        <p>00:00</p>
+                        <span><i class="fa fa-umbrella"></i> 10%</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div id="mainBloc2" class="lotOfData">
+            <span class="subBlocTitle">Humidité</span>
+            <div class="TimeCart" style="height: 9em;">
+                <div class="detailBox">
+                    <div class="data">
+                        <b class="m1"></b>
+                        <b class="m2"><span>28%</span><span></span></b>
+                        <b class="m3"></b>
+                    </div>
+                </div>
+                <div class="detailBox">
+                    <div class="data">
+                        <b class="m1"><span>29%</span><span></span></b>
+                        <b class="m2"></b>
+                        <b class="m3"></b>
+                    </div>
+                </div>
+                <div class="detailBox">
+                    <div class="data">
+                        <b class="m1"></b>
+                        <b class="m2"></b>
+                        <b class="m3"><span>27%</span><span></span></b>
+                    </div>
+                </div>
+                <div class="detailBox">
+                    <div class="data">
+                        <b class="m1"></b>
+                        <b class="m2"></b>
+                        <b class="m3"><span>27%</span><span></span></b>
+                    </div>
+                </div>
+                <div class="detailBox">
+                    <div class="data">
+                        <b class="m1"></b>
+                        <b class="m2"></b>
+                        <b class="m3"><span>27%</span><span></span></b>
+                    </div>
+                </div>
+                <div class="detailBox">
+                    <div class="data">
+                        <b class="m1"></b>
+                        <b class="m2"></b>
+                        <b class="m3"><span>27%</span><span></span></b>
+                    </div>
+                </div>
+                <div class="detailBox">
+                    <div class="data">
+                        <b class="m1"></b>
+                        <b class="m2"></b>
+                        <b class="m3"><span>27%</span><span></span></b>
+                    </div>
+                </div>
+                <div class="detailBox">
+                    <div class="data">
+                        <b class="m1"></b>
+                        <b class="m2"></b>
+                        <b class="m3"><span>27%</span><span></span></b>
+                    </div>
+                </div>
+                <div class="detailBox">
+                    <div class="data">
+                        <b class="m1"></b>
+                        <b class="m2"></b>
+                        <b class="m3"><span>27%</span><span></span></b>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div id="mainBloc2" class="lotOfData">
+            <span class="subBlocTitle">Luminosité</span>
+            <div class="TimeCart" style="height: 9em;">
+                <div class="detailBox">
+                    <div class="data">
+                        <b class="m1"></b>
+                        <b class="m2"><span>28%</span><span></span></b>
+                        <b class="m3"></b>
+                    </div>
+                </div>
+                <div class="detailBox">
+                    <div class="data">
+                        <b class="m1"><span>29%</span><span></span></b>
+                        <b class="m2"></b>
+                        <b class="m3"></b>
+                    </div>
+                </div>
+                <div class="detailBox">
+                    <div class="data">
+                        <b class="m1"></b>
+                        <b class="m2"></b>
+                        <b class="m3"><span>27%</span><span></span></b>
+                    </div>
+                </div>
+                <div class="detailBox">
+                    <div class="data">
+                        <b class="m1"></b>
+                        <b class="m2"></b>
+                        <b class="m3"><span>27%</span><span></span></b>
+                    </div>
+                </div>
+                <div class="detailBox">
+                    <div class="data">
+                        <b class="m1"></b>
+                        <b class="m2"></b>
+                        <b class="m3"><span>27%</span><span></span></b>
+                    </div>
+                </div>
+                <div class="detailBox">
+                    <div class="data">
+                        <b class="m1"></b>
+                        <b class="m2"></b>
+                        <b class="m3"><span>27%</span><span></span></b>
+                    </div>
+                </div>
+                <div class="detailBox">
+                    <div class="data">
+                        <b class="m1"></b>
+                        <b class="m2"></b>
+                        <b class="m3"><span>27%</span><span></span></b>
+                    </div>
+                </div>
+                <div class="detailBox">
+                    <div class="data">
+                        <b class="m1"></b>
+                        <b class="m2"></b>
+                        <b class="m3"><span>27%</span><span></span></b>
+                    </div>
+                </div>
+                <div class="detailBox">
+                    <div class="data">
+                        <b class="m1"></b>
+                        <b class="m2"></b>
+                        <b class="m3"><span>27%</span><span></span></b>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div id="mainBloc2" class="lotOfData" style="margin-bottom: 0.5em; ">
+            <span class="subBlocTitle">Vent</span>
+            <div class="TimeCart" style="height: 9em;">
+                <div class="detailBox">
+                    <div class="data">
+                        <b class="m1"></b>
+                        <b class="m2"><span>28m/s</span><span></span></b>
+                        <b class="m3"></b>
+                    </div>
+                </div>
+                <div class="detailBox">
+                    <div class="data">
+                        <b class="m1"><span>29m/s</span><span></span></b>
+                        <b class="m2"></b>
+                        <b class="m3"></b>
+                    </div>
+                </div>
+                <div class="detailBox">
+                    <div class="data">
+                        <b class="m1"></b>
+                        <b class="m2"></b>
+                        <b class="m3"><span>27m/s</span><span></span></b>
+                    </div>
+                </div>
+                <div class="detailBox">
+                    <div class="data">
+                        <b class="m1"></b>
+                        <b class="m2"></b>
+                        <b class="m3"><span>27m/s</span><span></span></b>
+                    </div>
+                </div>
+                <div class="detailBox">
+                    <div class="data">
+                        <b class="m1"></b>
+                        <b class="m2"></b>
+                        <b class="m3"><span>27m/s</span><span></span></b>
+                    </div>
+                </div>
+                <div class="detailBox">
+                    <div class="data">
+                        <b class="m1"></b>
+                        <b class="m2"></b>
+                        <b class="m3"><span>27m/s</span><span></span></b>
+                    </div>
+                </div>
+                <div class="detailBox">
+                    <div class="data">
+                        <b class="m1"></b>
+                        <b class="m2"></b>
+                        <b class="m3"><span>27m/s</span><span></span></b>
+                    </div>
+                </div>
+                <div class="detailBox">
+                    <div class="data">
+                        <b class="m1"></b>
+                        <b class="m2"></b>
+                        <b class="m3"><span>27m/s</span><span></span></b>
+                    </div>
+                </div>
+                <div class="detailBox">
+                    <div class="data">
+                        <b class="m1"></b>
+                        <b class="m2"></b>
+                        <b class="m3"><span>27m/s</span><span></span></b>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="author">
+            <span>Source: Station météo ESP8266 - AI&IoT Club (TP1)</span>
+            <span>IFRI 2022</span>
+        </div>
+    </main>
+    <footer>
+        
+    </footer>
+</body>
+</html>
